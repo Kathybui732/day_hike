3bd6f38bdfcd1feb008cf091fb1a262539a1a55f
Kathy Bui <kathy.bui1992@gmail.com>
2020-07-21 21:49:09 -0600

    Finish user story 3

---
+++ spec/features/trips/show_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe "Trip Show Page" do
+	before :each do
+    @trip_1 = Trip.create(name: "Trip 1")
+    @trail_1 = Trail.create(name: "Garden of the Gods", address: "123 Hike Trail", length: 2)
+    @trail_2 = Trail.create(name: "Incline", address: "123 Hike Trail", length: 3)
+    TripTrail.create(trip: @trip_1, trail: @trail_1)
+    TripTrail.create(trip: @trip_1, trail: @trail_2)
+	end
+
+  it "a list of trails included in the trip" do
+    visit "/trips/#{@trip_1.id}"
+    expect(page).to have_content(@trail_1.name)
+    expect(page).to have_content(@trail_2.name)
+  end
+
+  it "displays the name, address, and length for each trail" do
+    visit "/trips/#{@trip_1.id}"
+    expect(page).to have_content(@trail_1.name)
+    expect(page).to have_content(@trail_2.name)
+    expect(page).to have_content(@trail_1.address)
+    expect(page).to have_content(@trail_2.address)
+    expect(page).to have_content(@trail_1.length)
+    expect(page).to have_content(@trail_2.length)
+  end
+end
